c02571c28b7081ee0f8b85cbb1d0a9dc36e5c357
Hilmi Raditya <raditya113@gmail.com>
2020-06-08 04:05:08 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('percobaan');
-    // return view('welcome');
+    // return view('percobaan');
+    return view('welcome');
     // return redirect('/home');
 });
 
