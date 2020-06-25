295291f31f755f5bc74f0d5b1a83cc7d2f53b139
Hilmi Raditya <raditya113@gmail.com>
2020-06-26 04:23:15 +0700

    bug fix

--- app/Http/Controllers/ShortLinkController.php
+++ app/Http/Controllers/ShortLinkController.php
@@ -9,7 +9,7 @@ class ShortLinkController extends Controller
 {
     public function home()
     {
-        return view('home2')->with($data);
+        return view('home2');
     }
 
     public function notFound()
